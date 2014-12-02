afd292dee10246be414aa5cbeb10574c60511f91
Thomas Lauer <thomaslauer@users.noreply.github.com>
2014-12-02 13:58:55 -0600

    Update GameComponent.java

--- src/com/base/core/components/GameComponent.java
+++ src/com/base/core/components/GameComponent.java
@@ -30,56 +30,92 @@ public class GameComponent {
 		}
 	}
 	
-	// a reference to the parent object
+	/**
+	 * A reference to the parent GameObject
+	 */
 	protected GameObject parentObject;
 	
-	// boolean which will enable or disable the component
+	/**
+	 * enables or disables the component
+	 */
 	private boolean isEnabled = true;	
 	
-	// a name for the component
+	/**
+	 * the individual component name
+	 */
 	public String name;
 	
-	// a reference to the parent object transform
+	/**
+	 * reference to the parent object transform
+	 */
 	public Transform parentTransform;
 	
-	// a reference to the engine
+	/**
+	 * reference to the game engine
+	 */
 	public Engine engine;
 	
-	// constructor with name
+	/**
+	 * constructor that takes a name
+	 * @param name the name
+	 */
 	public GameComponent(String name)
 	{ this.name = name; }
 	
-	// no argument constructor, for nice constructing uses
+	/**
+	 * no argument constructor
+	 */
 	public GameComponent()
 	{ this(""); }
 	
-	// method that gets called before the first frame, but after all the objects are done
+	/**
+	 * method that gets called before the first frame, but after the scene is built
+	 */
 	public void init(){}
 	
-	// method to handle input
+	/**
+	 * method to handle input
+	 */
 	public void input(){}
 	
-	// method to handle updating the game
+	/**
+	 * method to update the game
+	 */
 	public void update(){}
 	
-	// method to render things
+	/**
+	 * method to render things
+	 */
 	public void render(){}
 	
-	// getters and setters for isEnabled
+	/**
+	 * gets if the component is enabled
+	 * @return if the component is enabled
+	 */
 	public boolean isEnabled() {
 		return isEnabled;
 	}
-
+	
+	/**
+	 * sets if the component is enabled
+	 * @param isEnabled if it is enabled
+	 */
 	public void setEnabled(boolean isEnabled) {
 		this.isEnabled = isEnabled;
 	}
 	
-	// getter for the parent object
+	/**
+	 * getter for the parent object
+	 * @return the parent object
+	 */
 	public GameObject getParentObject() {
 		return parentObject;
 	}
 	
-	// setter for the parent object, used when adding the component to an object
+	/**
+	 * sets the parent object of the component, and adds it to the engine
+	 * @param parentObject the parent object
+	 */
 	public void setParentObject(GameObject parentObject) {
 		this.parentObject = parentObject;
 		parentTransform = parentObject.transform;
