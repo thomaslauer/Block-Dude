1b4517a6884e2fa4fb1ef9812e3f0fa688f1d90d
Thomas Lauer <thomaslauer01@gmail.com>
2015-01-29 20:05:10 -0600

    Revert "Update Game.java"
    This reverts commit 591350b87dbc24523bcb1e2fb121a5d44211d685.
    
    Conflicts:
    	src/com/base/game/Game.java

---
+++ src/com/base/game/Game.java
@@ -0,0 +1,24 @@
+/*
+ * Game class
+ * Handles all the game stuff
+ */
+
+package com.base.game;
+
+import com.base.core.AbstractGame;
+import com.base.core.Bitmap;
+import com.base.core.GameObject;
+import com.base.core.components.*;
+import com.base.game.components.WorldComponent;
+
+public class Game extends AbstractGame{
+	public void init()
+	{
+		rootObject.addObject(new GameObject("face").setPosition(100, 100, 0)
+				.addComponent(new ReverseKeyboardMoveComponent())
+				.addComponent(new WorldComponent("GameFile1")));
+		
+		Bitmap m = new Bitmap("bitmap");
+		System.out.println(m.getR(0, 0));
+	}
+}
